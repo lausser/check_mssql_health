f825683f5883ded52bbb18dbdb2135b37492fded
Gerhard Lausser <gerhard.lausser@consol.de>
2020-11-12 16:52:26 +0100

    bugfixing and improving availability-group-health. Sponsored by Audi. If you like this plugin, buy one of their cars. And one for your wife

--- plugins-scripts/Classes/MSSQL/Component/AvailabilitygroupSubsystem.pm
+++ plugins-scripts/Classes/MSSQL/Component/AvailabilitygroupSubsystem.pm
@@ -11,13 +11,16 @@ sub init {
   };
   if ($self->mode =~ /server::availabilitygroup::status/) {
     if ($self->version_is_minimum("11.x")) {
-      my $columns = [qw(server_name group_id name primary_replica primary_recovery_health_desc
-          secondary_recovery_health_desc synchronization_health_desc)];
       my $avgfilter = sub {
         my $o = shift;
         $self->filter_name($o->{name});
       };
-      my $sql = q{
+      my $columnsag = [qw(server_name group_id name
+          primary_replica primary_recovery_health_desc
+          secondary_recovery_health_desc synchronization_health_desc
+      )];
+      # returns 1 line with availability group
+      my $sqlag = q{
         SELECT
           @@ServerName,
           [ag].[group_id],
@@ -37,19 +40,54 @@ sub init {
         ON
           [ag].[group_id] = [gs].[group_id]
       };
-      my $resql = q{
-        select @@ServerName,* from [master].[sys].[dm_hadr_availability_replica_states]
+      my $columnsrs = [qw(server_name group_id name database_name
+          synchronization_state_desc replica_id group_database_id
+      )];
+      # returns n lines with availability group +
+      #   database (probably always the same, i never saw different records) +
+      #   replica_id (which is unique), so name+replica_id are unique here
+      my $sqlrs = q{
+        SELECT
+          @@ServerName,
+          [ag].[group_id],
+          [ag].[name],
+          [db].[name] AS Databasename,
+          [rs].[synchronization_state_desc],
+          [rs].[replica_id],
+          [rs].[group_database_id]
+        FROM [master].[sys].[availability_groups] AS [ag]
+        INNER JOIN [master].[sys].[dm_hadr_availability_group_states] AS [gs] ON [ag].[group_id] = [gs].[group_id]
+        INNER JOIN [master].[sys].[dm_hadr_database_replica_states] AS [rs] ON [ag].[group_id] = [rs].[group_id]
+        INNER JOIN [master].[sys].[databases] AS [db] ON [rs].[database_id] = [db].[database_id]
       };
-      my $recolumns = [qw(server_name replica_id group_id is_local role role_desc operational_state
-          operational_state_desc connected_state connected_state_desc recovery_health
-          recovery_health_desc synchronization_health synchronization_health_desc
-          last_connect_error_number last_connect_error_description
-          last_connect_error_timestamp)];
+
       $self->get_db_tables([
-          ['avgroups', $sql, 'Classes::MSSQL::Component::AvailabilitygroupSubsystem::Availabilitygroup', $avgfilter, $columns],
-          # vielleicht spaeter mal, um mehr details zu holen
-          #['regroups', $resql, 'Classes::MSSQL::Component::AvailabilitygroupSubsystem::Replicastate', $avgfilter, $recolumns],
+          ['avgroups', $sqlag, 'Classes::MSSQL::Component::AvailabilitygroupSubsystem::Availabilitygroup', $avgfilter, $columnsag],
+          ['replicas', $sqlrs, 'Monitoring::GLPlugin::DB::TableItem', $avgfilter, $columnsrs],
       ]);
+      foreach my $avgroup (@{$self->{avgroups}}) {
+        foreach my $replica (@{$self->{replicas}}) {
+          if ($avgroup->{name} eq $replica->{name}) {
+            push(@{$avgroup->{replicas}}, $replica);
+          }
+        }
+      }
+      delete $self->{replicas};
+    }
+  }
+}
+
+sub check {
+  my ($self) = @_;
+  if ($self->mode =~ /server::availabilitygroup::status/) {
+    if ($self->version_is_minimum("11.x")) {
+      if (! scalar(@{$self->{avgroups}})) {
+        $self->add_ok("no availability groups found");
+      } else {
+        foreach (@{$self->{avgroups}}) {
+          $_->check();
+        }
+      }
     } else {
       $self->add_ok(sprintf 'your version %s is too old, availability group monitoring is not possible', $self->get_variable('version'));
     }
@@ -65,16 +103,37 @@ package Classes::MSSQL::Component::AvailabilitygroupSubsystem::Availabilitygroup
 our @ISA = qw(Monitoring::GLPlugin::DB::TableItem);
 use strict;
 
+sub finish {
+  my ($self) = @_;
+  $self->{replicas} = [];
+}
+
 sub check {
   my ($self) = @_;
   if ($self->mode =~ /server::availabilitygroup::status/) {
-    $self->add_info(sprintf 'availability group %s has synch. status %s', $self->{name},
-        lc $self->{synchronization_health_desc});
-    if ($self->{server_name} ne $self->{primary_replica}) {
-      $self->add_ok(sprintf 'this is is a secondary replica if group %s. for a reliable status you have to ask the primary replica',
+    $self->add_info(
+        sprintf 'availability group %s synchronization is %s',
+        $self->{name}, lc $self->{synchronization_health_desc}
+    );
+    if (lc $self->{server_name} ne lc $self->{primary_replica}) {
+      $self->add_ok(sprintf 'this is is a secondary replica of group %s. for a reliable status you have to ask the primary replica',
           $self->{name});
     } elsif ($self->{synchronization_health_desc} eq 'HEALTHY') {
-      $self->add_ok();
+      my $nok = scalar(grep { $_->{synchronization_state_desc} !~ /^SYNCHRONIZ/ } @{$self->{replicas}});
+      if (! $nok) {
+        $self->add_ok();
+      } else {
+        foreach my $replica (@{$self->{replicas}}) {
+          if ($replica->{synchronization_state_desc} ne 'SYNCHRONIZING' &&
+              $replica->{synchronization_state_desc} ne 'SYNCHRONIZED') {
+            $self->add_info(sprintf 'replica %s@%s has synchronization state %s',
+                $replica->{replica_id}, $self->{name},
+                lc $replica->{synchronization_state_desc}
+            );
+            $self->add_critical();
+          }
+        }
+      }
     } elsif ($self->{synchronization_health_desc} eq 'PARTIALLY_HEALTHY') {
       $self->add_warning();
     } elsif ($self->{synchronization_health_desc} eq 'NOT_HEALTHY') {
